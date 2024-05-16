8bd1f96fe1bdf718019d80962ae00cd42c080d64
Monica Munoz Sanchez <monmunoz@c1r7s5.42madrid.com>
2024-05-16 13:56:55 +0200

    first attempt of understanding

---
+++ get_next_line.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: monmunoz <monmunoz@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/16 13:45:19 by monmunoz          #+#    #+#             */
+/*   Updated: 2024/05/16 13:52:06 by monmunoz         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <fcntl.h>
+
+# define BUFFER_SIZE 5
+
+char	*get_next_line(int fd);
+
+#endif
