d83a2f920589e2021776d942e7aba8a737b180e3
Monica Munoz Sanchez <monmunoz@student.42madrid.com>
2024-06-26 12:54:04 +0200

    reorganizando retry

--- get_next_line.c
+++ get_next_line.c
@@ -6,81 +6,15 @@
 /*   By: monmunoz <monmunoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:27:10 by monmunoz          #+#    #+#             */
-/*   Updated: 2024/06/17 21:32:30 by monmunoz         ###   ########.fr       */
+/*   Updated: 2024/06/18 20:01:32 by monmunoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-char	*leftover(char *v_est, char *text, int length)
-{
-	char	*temp;
-
-	temp = ft_strjoin(v_est, text, length);
-	free(text);
-	text = temp;
-	free(v_est);
-	return (text);
-}
-
-char	*pieces(char *cut, char *text, size_t pos)
-{
-	size_t	i;
-
-	i = 0;
-	if (!cut || !text)
-		return (NULL);
-	while (i < pos)
-	{
-		cut[i] = text[i];
-		i++;
-	}
-	cut[pos] = '\0';
-	return (cut);
-}
-
-char	*divided(char *text, int fd, int length)
-{
-	char	buffer[BUFFER_SIZE + 1];
-	size_t	bytes_read;
-	char	*new_text;
-
-	bytes_read = read(fd, buffer, BUFFER_SIZE);
-	if (bytes_read <= 0)
-		return (text);
-	buffer[bytes_read] = '\0';
-	new_text = ft_strjoin(text, buffer, (bytes_read + length));
-	free(text);
-	return (new_line(new_text, (bytes_read + length), fd));
-}
-
 char	*new_line(char *text, int length, int fd)
 {
-	int			count;
-	static char	*v_est;
-	char		*clean_line;
-
-	count = 0;
-	if (v_est != NULL)
-	{
-		length = length + ft_strlen(v_est);
-		text = leftover(v_est, text, length);
-		v_est = NULL;
-	}
-	while (text[count] != '\n' && count < length)
-		count++;
-	if (text[count] != '\n')
-		text = divided(text, fd, length);
-	else
-	{
-		v_est = (char *)(malloc(((length - count) + 1) * sizeof(char)));
-		v_est = pieces(v_est, &text[count + 1], ((size_t)(length - count)));
-		clean_line = (char *)(malloc((count + 1) * sizeof(char)));
-		clean_line = pieces(clean_line, text, count);
-		free (text);
-		text = clean_line;
-	}
-	return (text);
+	return (new_text);
 }
 
 char	*get_next_line(int fd)
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,46 +6,22 @@
 /*   By: monmunoz <monmunoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 13:44:55 by monmunoz          #+#    #+#             */
-/*   Updated: 2024/06/17 21:33:30 by monmunoz         ###   ########.fr       */
+/*   Updated: 2024/06/18 21:02:45 by monmunoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-static char	*ft_strcat(char *dest, const char *src)
+char	*ft_strchr(char *text, int counter)
 {
-	int	j;
 	int	i;
 
 	i = 0;
-	j = 0;
-	while (dest[i])
+	while (text[i] != '\0')
+	{
+		if (text[i] == (char)counter)
+			return (((char *)text) + i);
 		i++;
-	while (src[j])
-		dest[i++] = src[j++];
-	dest[i] = '\0';
-	return (dest);
-}
-
-char	*ft_strjoin(char *s1, const char *s2, int size)
-{
-	char	*s3;
-
-	s3 = (char *)malloc((size + 1) * sizeof(char));
-	if (!s3)
-		return (NULL);
-	s3[0] = '\0';
-	s3 = ft_strcat(s3, s1);
-	s3 = ft_strcat(s3, s2);
-	return ((char *)s3);
-}
-
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (s[len])
-		len++;
-	return (len);
+	}
+	return (NULL);
 }
