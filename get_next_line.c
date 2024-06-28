aad746f238d0dc784764caa4fd8e8de5434bdabb
Monica Munoz Sanchez <monmunoz@student.42madrid.com>
2024-06-28 20:15:36 +0200

    gestion fd, read_file y aumentar el buffer, leaks

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,12 @@
 /*   By: monmunoz <monmunoz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/16 12:27:10 by monmunoz          #+#    #+#             */
-/*   Updated: 2024/06/27 18:31:13 by monmunoz         ###   ########.fr       */
+/*   Updated: 2024/06/28 20:11:53 by monmunoz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 
-//char	*new_line(char *buf)
 char	*get_next_line(int fd)
 {
 	char		*buf;
@@ -21,30 +20,27 @@ char	*get_next_line(int fd)
 
 	if (fd < 0 || BUFFER_SIZE <= 0 || BUFFER_SIZE >= INT_MAX)
 		return (NULL);
-	else
+	buf = (char *)(malloc((BUFFER_SIZE + 1) * sizeof(char)));
+	if (!buf)
+		return (NULL);
+	read_file = read (fd, buf, BUFFER_SIZE);
+	if (read_file <= 0)
 	{
-		buf = (char *)(malloc((BUFFER_SIZE + 1) * sizeof(char)));
-		if (!buf)
-			return (NULL);
-		read_file = read (fd, buf, BUFFER_SIZE);
-		if (read_file <= 0)
-		{
-			free (buf);
-			return (NULL);
-		}
-		buf[read_file] = '\0';
-		reserved = ft_strchr(buf, '\n');
-		printf("size %zu\n", read_file);
-		if (!reserved)
-		{
-			read_file = read(fd, buf, (read_file + BUFFER_SIZE));
-			buf = (char *)(malloc((read_file + BUFFER_SIZE + 1) * sizeof(char)));			
-			printf("size %zu\n", read_file);
-			
-		}
-		reserved = ft_strchr(buf, '\n');
-		printf("RESERVED %s\n", reserved);
-		
+		free (buf);
+		return (NULL);
 	}
+	buf[read_file] = '\0';
+	reserved = ft_strchr(buf, '\n');
+	while (!reserved && read_file)
+	{
+		reserved = (char *)(malloc((read_file + BUFFER_SIZE + 1) * sizeof(char)));
+		reserved = ft_memmove(reserved, buf, read_file);
+		read_file += read(fd, &reserved[read_file], BUFFER_SIZE);
+		printf("reserverd: %s %zu\n", buf, read_file);
+		reserved[read_file] = '\0';
+		reserved = ft_strchr(buf, '\n');
+		free (reserved);
+	}
+	printf("RESERVED %s\n", reserved);
 	return (reserved);
 }
